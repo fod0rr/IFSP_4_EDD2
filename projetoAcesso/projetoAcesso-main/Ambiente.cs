bb109a35b130a14d305a6634f5ba289617e1210b
Edu <130316981+fod0rr@users.noreply.github.com>
2025-01-29 16:19:37 -0300

    Add files via upload

---
+++ projetoAcesso/projetoAcesso-main/Ambiente.cs
@@ -0,0 +1,17 @@
+//Desenvolvido por Maria Eduarda Fodor
+
+using System;
+using System.Collections.Generic;
+
+public class Ambiente {
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public Queue<Log> Logs { get; set; } = new Queue<Log>();
+
+    public void RegistrarLog(Log log) {
+        if (Logs.Count >= 100) {
+            Logs.Dequeue();
+        }
+        Logs.Enqueue(log);
+    }
+}
